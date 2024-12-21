5f864fcf2b8034deaf18df8e46f7ed6f50610f2e
Guevaer Arnaud <guevaer8@gmail.com>
2024-12-21 18:01:53 +0100

    finishing project page

--- client/src/components/Page/Page3.tsx
+++ client/src/components/Page/Page3.tsx
@@ -1,48 +1,78 @@
 import { motion } from "framer-motion";
 import "./Page.css";
+import { useState } from "react";
 
 const Page3 = () => {
   const projects = [
     {
-      img: "",
-      name: "Code Quest Academy (en cours)",
+      img: "./src/assets/images/CQA.png",
+      gif: "",
+      name: "Code Quest Academy",
       description:
         "Un jeu éducatif interactif mêlant investigation et apprentissage, où les joueurs incarnent un étudiant en développement web, résolvant des énigmes pour maîtriser HTML, CSS, JavaScript, et autres technologies, tout en explorant un narratif inspiré de la pop culture.",
       technology: "React, TypeScript, Node.js, Express, SQL.",
-      url: "",
+      url: "https://github.com/WildCodeSchool-2024-09/JS-lille-P3-code-quest-academy",
     },
     {
-      img: "",
+      img: "./src/assets/images/zwarrior.png",
+      gif: "./src/assets/images/zwarrior.gif",
       name: "Z Warriors Clicker",
       description:
         "Un jeu incremental sur l'univers de Dragon Ball, où les joueurs cliquent pour accumuler de la puissance, débloquer des transformations et techniques puissantes, et vaincre des ennemis emblématiques.",
       technology: "React, TypeScript,",
-      url: "",
+      url: "https://github.com/Arnaud6216/Z-Warriors-Clicker",
     },
     {
-      img: "",
+      img: "./src/assets/images/feedgood.png",
+      gif: "./src/assets/images/feedgood.gif",
       name: "Feed good",
       description:
         "Réalisé lors d'un hackathon, Feed good est une application basée sur le thème de la bienveillance, où l'utilisateur peut effectuer des actions positives pour lui-même et pour les autres.",
       technology: "React, TypeScript, Node.js, Express.",
-      url: "",
+      url: "https://github.com/WildCodeSchool-2024-09/lille-protojam",
     },
     {
-      img: "",
+      img: "./src/assets/images/wildaway.png",
+      gif: "./src/assets/images/wildaway.gif",
       name: "Wild away",
       description:
         "Une application de réservation d'hotêls et de restaurants partout dans le monde avec une carte interactive.",
       technology: "React, TypeScript, Node.js, Express.",
-      url: "",
+      url: "https://github.com/WildCodeSchool-2024-09/LILLE-0924-PROJECT2-G2-WILD-AWAY",
     },
     {
-      img: "",
+      img: "./src/assets/images/fantizz.png",
+      gif: "./src/assets/images/fantizz.gif",
       name: "Fantizz",
       description: "Un quiz intéractif sur un thème humoristique.",
       technology: "HTML, CSS, Javascript.",
-      url: "",
+      url: "https://github.com/Foub404/projet-p1",
     },
   ];
+
+  const [selectedProject, setSelectedProject] = useState<{
+    img: string;
+    gif: string;
+    name: string;
+    description: string;
+    technology: string;
+    url: string;
+  } | null>(null);
+
+  const handleProjectDetails = (project: {
+    img: string;
+    gif: string;
+    name: string;
+    description: string;
+    technology: string;
+    url: string;
+  }) => {
+    setSelectedProject(project);
+  };
+
+  const handleCloseDetails = () => {
+    setSelectedProject(null);
+  };
 
   return (
     <motion.div
@@ -53,18 +83,63 @@ const Page3 = () => {
       transition={{ duration: 0.6 }}
     >
       <div className="page-content">
-        <h2>Projets</h2>
+        <h2>Mes projets</h2>
         <section className="projects-container">
           {projects.map((project) => (
             <article key={project.name} className="project">
-              <img src={project.img} alt={project.name} />
+              <img
+                className="projects-img"
+                src={project.img}
+                alt={project.name}
+                onClick={() => handleProjectDetails(project)}
+              />
               <h3>{project.name}</h3>
-              <p>{project.description}</p>
-              <p>{project.technology}</p>
             </article>
           ))}
         </section>
       </div>
+
+      {selectedProject && (
+        <div
+          className="popup-overlay"
+          onClick={handleCloseDetails}
+          onKeyUp={(e) => e.key === "Escape" && handleCloseDetails()}
+        >
+          <motion.div
+            className="popup-content"
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.8, opacity: 0 }}
+            transition={{ duration: 0.4 }}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button
+              type="button"
+              className="close-button"
+              onClick={handleCloseDetails}
+            >
+              X
+            </button>
+            <img
+              src={selectedProject.gif || selectedProject.img}
+              alt={selectedProject.name}
+              className="popup-img"
+            />
+            <h2>{selectedProject.name}</h2>
+            <p className="popup-text">{selectedProject.description}</p>
+            <p className="popup-text">
+              <strong>Technologies :</strong> {selectedProject.technology}
+            </p>
+            <a
+              href={selectedProject.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Voir sur Github
+            </a>
+          </motion.div>
+        </div>
+      )}
     </motion.div>
   );
 };
